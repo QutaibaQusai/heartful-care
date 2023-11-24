34f8a6eae6c16c9abc3837d6080ab8d4bf41c75e
Qutaiba Qusai Makahleh <qutaiba.makahleh@gmail.com>
2023-11-24 18:35:49 +0300

    Edited user profile

--- lib/Profile/account.dart
+++ lib/Profile/account.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:test/home.dart';
 //import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 //import 'package:google_fonts/google_fonts.dart';
@@ -16,36 +17,82 @@ class _Account extends State<Account> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
+        toolbarHeight: 100,
         title: Text(
           'Profile',
-          style: TextStyle(fontSize: 22, color: Colors.white),
+          style: TextStyle(
+            fontSize: 22,
+            color: Colors.white,
+            fontFamily: GoogleFonts.poppins().fontFamily,
+          ),
         ),
+
         centerTitle: true, // Center the title
         backgroundColor: Color(0xFF1C8892),
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back),
-          onPressed: () {
-            Navigator.of(context)
-                .push(MaterialPageRoute(builder: (context) => Home()));
-          },
+        leading: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: CircleAvatar(
+            radius: 20,
+            backgroundColor: Colors.white,
+            child: IconButton(
+              icon: Icon(Icons.arrow_back),
+              onPressed: () {
+                Navigator.of(context)
+                    .push(MaterialPageRoute(builder: (context) => Home()));
+              },
+              color: Color(0xFF1C8892),
+            ),
+          ),
         ),
+
         actions: [
-          IconButton(
-            icon: Icon(Icons.edit),
-            onPressed: () {},
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: CircleAvatar(
+              radius: 20,
+              backgroundColor: Colors.white,
+              child: IconButton(
+                icon: Icon(Icons.edit),
+                color: Color(0xFF1C8892),
+                onPressed: () {},
+              ),
+            ),
           ),
         ],
       ),
       body: Column(
         children: [
-          Stack(alignment: Alignment.center, children: [
-            Container(
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height / 4.0,
-              color: Color(0xFF1C8892),
-            ),
-          ]),
+          Stack(
+            children: [
+              Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height / 4.0,
+                color: Color(0xFF1C8892),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(22, 0, 22, 0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(30),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 5,
+                        blurRadius: 7,
+                        offset: Offset(0, 3), // changes position of shadow
+                      ),
+                    ],
+                  ),
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height / 1.45,
+                  child: Column(children: []),
+                ),
+              )
+            ],
+          ),
         ],
       ),
     );
