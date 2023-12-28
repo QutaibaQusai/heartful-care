c50bd27eb5c6431f21775743d64618e6b10c6607
Saraabuzaid02 <2010144@std.aum.edu.jo>
2023-12-28 20:11:42 +0300

    location

--- lib/sections/medical_devices.dart
+++ lib/sections/medical_devices.dart
@@ -1,320 +1,193 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:test/sections/AllDevicesPage.dart';
 
-class Medical extends StatelessWidget {
-  const Medical({Key? key}) : super(key: key);
+class Medical extends StatefulWidget {
+  @override
+  State<Medical> createState() => _MedicalState();
+}
+
+class _MedicalState extends State<Medical> {
+  String? selectedLocation;
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(),
-        backgroundColor: Colors.white,
-        body: SingleChildScrollView(
-          // Wrap your Column with SingleChildScrollView
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "Hi, Welcome to",
-                  style: TextStyle(
-                    fontSize: 15,
-                    color: Colors.grey[600],
-                    fontFamily: GoogleFonts.poppins().fontFamily,
-                  ),
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            SizedBox(height: 30),
+            Text(
+              'Location',
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.grey,
+              ),
+            ),
+            SizedBox(
+              height: 8,
+            ), // Add some space between "Location" and the next row
+            Row(
+              children: <Widget>[
+                Icon(
+                  Icons.location_on,
+                  color: Color(0xFF1C8892),
+                  // You can change the color of the location icon
                 ),
-                Text(
-                  "Medical store",
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                    fontFamily: GoogleFonts.poppins().fontFamily,
-                  ),
-                ),
-                SizedBox(
-                  height: 12,
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20.0),
-                    border: Border.all(color: Color(0xFF1C8892)),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.fromLTRB(9, 0, 9, 0),
-                    child: Row(
-                      children: [
-                        const Icon(FontAwesomeIcons.search),
-                        SizedBox(width: 8),
-                        Flexible(
-                          child: TextField(
-                            decoration: InputDecoration(
-                              hintStyle: TextStyle(color: Colors.grey),
-                              hintText: 'Search ',
-                              border: InputBorder.none,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                SizedBox(height: 16),
-                Container(
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Color(0xFF1C8892)),
-                    borderRadius: BorderRadius.circular(12),
-                    color: Color(0xFF1C8892),
-                  ),
-                  padding: const EdgeInsets.all(16),
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            "Looking for your desired\ndevices quickly?",
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 16,
-                              fontFamily: GoogleFonts.poppins().fontFamily,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          SizedBox(height: 8),
-                          Row(
-                            children: [
-                              FaIcon(
-                                FontAwesomeIcons.check,
-                                color: Colors.white,
-                                size: 16,
-                              ),
-                              SizedBox(width: 8),
-                              Text(
-                                "Best In price",
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 16,
-                                  fontFamily: GoogleFonts.poppins().fontFamily,
-                                ),
-                              ),
-                            ],
-                          ),
-                          SizedBox(height: 8),
-                          Row(
-                            children: [
-                              FaIcon(
-                                FontAwesomeIcons.check,
-                                color: Colors.white,
-                                size: 16,
-                              ),
-                              SizedBox(width: 8),
-                              Text(
-                                "Very Quick delivery",
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 16,
-                                  fontFamily: GoogleFonts.poppins().fontFamily,
-                                ),
-                              ),
-                            ],
-                          ),
-                          SizedBox(height: 8),
-                          Row(
-                            children: [
-                              FaIcon(
-                                FontAwesomeIcons.check,
-                                color: Colors.white,
-                                size: 16,
-                              ),
-                              SizedBox(width: 8),
-                              Text(
-                                "Get Refund policy",
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 16,
-                                  fontFamily: GoogleFonts.poppins().fontFamily,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                      SizedBox(width: 16),
-                      Container(
-                        width: 60,
-                        height: 60,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(12),
-                          image: DecorationImage(
-                            image: AssetImage(""),
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                SizedBox(height: 16),
-                SizedBox(
-                  height: 40,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        "Devices Categories",
-                        style: TextStyle(
-                          color: Color(0xFF1C8892),
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                          fontFamily: GoogleFonts.poppins().fontFamily,
-                        ),
-                      ),
-                      /*GestureDetector(
-                        onTap: () {
-                          // Add your onTap logic here
-                        },
-                        child: Text(
-                          "See All",
-                          style: TextStyle(
-                            color: Colors.grey,
-                            fontSize: 16,
-                          ),
-                        ),
-                      ),*/
-                    ],
-                  ),
-                ),
-                SizedBox(height: 16),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    _buildCategoryBox("Buy", "images/pressure.png"),
-                    _buildCategoryBox("Rent", "images/weelchair2.png"),
-                    _buildCategoryBox("Buy/Rent", "images/device_home.png"),
-                  ],
-                ),
-
-                // Popular Devices Section
-                SizedBox(height: 16),
-                SizedBox(
-                  height: 40,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        "Popular Devices",
-                        style: TextStyle(
-                          color: Color(0xFF1C8892),
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                          fontFamily: GoogleFonts.poppins().fontFamily,
-                        ),
-                      ),
-                      GestureDetector(
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => AllDevicesPage()),
-                          );
-                        },
-                        child: Text(
-                          "See All",
-                          style: TextStyle(
-                            color: Colors.grey,
-                            fontSize: 16,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                SizedBox(height: 16),
-
-                // Two Large Scrollable Boxes
-                SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: Row(
-                    children: [
-                      _buildLargeBox("Pressure device", "images/pressure.png"),
-                      SizedBox(width: 16),
-                      _buildLargeBox("WeelChair", "images/weelchair2.png"),
-                    ],
+                SizedBox(width: 8),
+                // Add space between the icon and the dropdown
+                Expanded(
+                  child: DropdownButton<String>(
+                    isExpanded: true,
+                    hint: Text('Select Location'),
+                    value: selectedLocation,
+                    items: <String>['Amman,Jordan', 'Irbid', 'Madaba']
+                        .map((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                    onChanged: (String? value) {
+                      setState(() {
+                        selectedLocation = value;
+                      });
+                    },
+                    // onChanged is optional; you can remove it if you don't need to handle value changes
                   ),
                 ),
               ],
             ),
-          ),
+
+            SizedBox(
+              height: 12,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20.0),
+                border: Border.all(color: Color(0xFF1C8892)),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(9, 0, 9, 0),
+                child: Row(
+                  children: [
+                    const Icon(FontAwesomeIcons.search),
+                    SizedBox(width: 8),
+                    Flexible(
+                      child: TextField(
+                        decoration: InputDecoration(
+                          hintStyle: TextStyle(color: Colors.grey),
+                          hintText: 'Search here ',
+                          border: InputBorder.none,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 16),
+            Container(
+              decoration: BoxDecoration(
+                border: Border.all(color: Color(0xFF1C8892)),
+                borderRadius: BorderRadius.circular(12),
+                color: Color(0xFF1C8892),
+              ),
+              padding: const EdgeInsets.all(16),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Looking for your desired\ndevices quickly?",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                          fontFamily: GoogleFonts.poppins().fontFamily,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 8),
+                      Row(
+                        children: [
+                          FaIcon(
+                            FontAwesomeIcons.check,
+                            color: Colors.white,
+                            size: 16,
+                          ),
+                          SizedBox(width: 8),
+                          Text(
+                            "Best In price",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                              fontFamily: GoogleFonts.poppins().fontFamily,
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 8),
+                      Row(
+                        children: [
+                          FaIcon(
+                            FontAwesomeIcons.check,
+                            color: Colors.white,
+                            size: 16,
+                          ),
+                          SizedBox(width: 8),
+                          Text(
+                            "Very Quick delivery",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                              fontFamily: GoogleFonts.poppins().fontFamily,
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 8),
+                      Row(
+                        children: [
+                          FaIcon(
+                            FontAwesomeIcons.check,
+                            color: Colors.white,
+                            size: 16,
+                          ),
+                          SizedBox(width: 8),
+                          Text(
+                            "Get Refund policy",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                              fontFamily: GoogleFonts.poppins().fontFamily,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  SizedBox(width: 16),
+                  Container(
+                    width: 60,
+                    height: 60,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      image: DecorationImage(
+                        image: AssetImage(""),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 16),
+          ],
         ),
-      ),
-    );
-  }
-
-  Widget _buildCategoryBox(String text, String imagePath) {
-    return Container(
-      width: 100,
-      height: 100,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(8),
-        color: Color(0xFF1C8892),
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Image.asset(
-            imagePath,
-            width: 70,
-            height: 70,
-            fit: BoxFit.cover,
-          ),
-          SizedBox(height: 8),
-          Text(
-            text,
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 15,
-              fontFamily: GoogleFonts.poppins().fontFamily,
-            ),
-            textAlign: TextAlign.center,
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildLargeBox(String text, String imagePath) {
-    return Container(
-      width: 200,
-      height: 200,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(8),
-        color: Color(0xFF1C8892),
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Image.asset(
-            imagePath,
-            width: 100,
-            height: 100,
-            fit: BoxFit.cover,
-          ),
-          SizedBox(height: 8),
-          Text(
-            text,
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 15,
-              fontFamily: GoogleFonts.poppins().fontFamily,
-            ),
-            textAlign: TextAlign.center,
-          ),
-        ],
       ),
     );
   }
