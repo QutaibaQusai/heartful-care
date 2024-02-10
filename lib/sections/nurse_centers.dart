a2c9075359c9aa9c447b5cc53823a7b9c2456e15
Qutaiba Qusai Makahleh <qutaiba.makahleh@gmail.com>
2024-02-10 15:47:30 +0300

    modifing tile desgin

--- lib/sections/nurse_centers.dart
+++ lib/sections/nurse_centers.dart
@@ -11,139 +11,249 @@ class NurseCenters extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
+        backgroundColor: Color(0xFF1C8892),
         appBar: AppBar(
-          title: Text(
-            "Nurse centers",
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 19,
+          backgroundColor: Color(0xFF1C8892),
+          leading: IconButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            icon: Icon(
+              FontAwesomeIcons.chevronLeft,
+              color: Colors.white,
             ),
           ),
         ),
-        body: StreamBuilder<QuerySnapshot>(
-          stream: FirebaseFirestore.instance.collection('centers').snapshots(),
-          builder: (context, snapshot) {
-            if (!snapshot.hasData) {
-              return Center(
-                child: CircularProgressIndicator(),
-              );
-            }
+        body: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              height: MediaQuery.of(context).size.height / 6.5,
+              decoration: BoxDecoration(
+                color: Color(0xFF1C8892),
+              ),
+              child: Column(
+                // mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(child: Container()),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Text(
+                      "Centers available",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ],
+              ),
+              // child: Padding(
+              //   padding: const EdgeInsets.all(8.0),
+              //   child: Row(
+              //     children: [
+              //       Expanded(
+              //         child: Container(
+              //           decoration: BoxDecoration(
+              //             borderRadius: BorderRadius.circular(10),
+              //             color: Colors.white,
+              //           ),
+              //           child: Row(
+              //             children: [
+              //               Padding(
+              //                 padding: const EdgeInsets.all(8.0),
+              //                 child: Icon(
+              //                   Icons.search,
+              //                   color: Color(0xFF1C8892),
+              //                 ),
+              //               ),
+              //               Expanded(
+              //                 child: TextField(
+              //                   decoration: InputDecoration(
+              //                     hintText: "Search for nurse center",
+              //                     border: InputBorder.none,
+              //                   ),
+              //                 ),
+              //               ),
+              //               Padding(
+              //                 padding: const EdgeInsets.all(8.0),
+              //                 child: Icon(
+              //                   Icons.filter_list,
+              //                   color: Color(0xFF1C8892),
+              //                 ),
+              //               ),
+              //             ],
+              //           ),
+              //         ),
+              //       ),
+              //     ],
+              //   ),
+              // ),
+            ),
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(40),
+                  ),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: StreamBuilder<QuerySnapshot>(
+                    stream: FirebaseFirestore.instance
+                        .collection('centers')
+                        .snapshots(),
+                    builder: (context, snapshot) {
+                      if (!snapshot.hasData) {
+                        return Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      }
 
-            List<NurseCenter> nurseCenters = snapshot.data!.docs
-                .map((doc) =>
-                    NurseCenter.fromMap(doc.data() as Map<String, dynamic>))
-                .toList();
+                      List<NurseCenter> nurseCenters = snapshot.data!.docs
+                          .map((doc) => NurseCenter.fromMap(
+                              doc.data() as Map<String, dynamic>))
+                          .toList();
 
-            return ListView.builder(
-              itemCount: nurseCenters.length,
-              itemBuilder: (context, index) {
-                var center = nurseCenters[index];
-                return Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: InkWell(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => DetailedNurseCenter(
-                            centerName: center.name,
-                            operatingHours: center.operatingHours,
-                            centerDescription: center.description,
-                            centerUrlImageLogo: center.urlLogoImage,
-                            centerPhoneNumber: center.phoneNumber,
-                          ),
-                        ),
-                      );
-                    },
-                    child: Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: Colors.white,
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.grey.withOpacity(0.5),
-                            spreadRadius: 5,
-                            blurRadius: 7,
-                            offset: Offset(0, 3),
-                          ),
-                        ],
-                      ),
-                      width: double.infinity,
-                      height: MediaQuery.of(context).size.height / 6,
-                      child: Padding(
-                        padding: const EdgeInsets.all(5.0),
-                        child: Container(
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              Container(
-                                height: 150.0,
-                                width: 100.0,
+                      return ListView.builder(
+                        itemCount: nurseCenters.length,
+                        itemBuilder: (context, index) {
+                          var center = nurseCenters[index];
+                          return Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: InkWell(
+                              onTap: () {
+                                // Handle tile click here, e.g., navigate to the detailed page
+                                Navigator.push(
+                                  context,
+                                  PageRouteBuilder(
+                                    pageBuilder: (context, animation,
+                                        secondaryAnimation) {
+                                      return DetailedNurseCenter(
+                                        centerName: center.name,
+                                        operatingHours: center.operatingHours,
+                                        centerDescription: center.description,
+                                        centerUrlImageLogo: center.urlLogoImage,
+                                        centerPhoneNumber: center.phoneNumber,
+                                      );
+                                    },
+                                    transitionsBuilder: (context, animation,
+                                        secondaryAnimation, child) {
+                                      const begin = Offset(0.0, 1.0);
+                                      const end = Offset.zero;
+                                      const curve = Curves.easeInOutQuart;
+
+                                      var tween = Tween(begin: begin, end: end)
+                                          .chain(CurveTween(curve: curve));
+                                      var offsetAnimation =
+                                          animation.drive(tween);
+
+                                      return SlideTransition(
+                                        position: offsetAnimation,
+                                        child: child,
+                                      );
+                                    },
+                                  ),
+                                );
+                              },
+                              child: Container(
                                 decoration: BoxDecoration(
-                                  shape: BoxShape.circle,
+                                  borderRadius: BorderRadius.circular(20),
+                                  color: Colors.white,
+                                  boxShadow: [
+                                    BoxShadow(
+                                      color: Colors.grey.withOpacity(0.5),
+                                      spreadRadius: 2,
+                                      blurRadius: 1,
+                                      offset: Offset(0, 3),
+                                    ),
+                                  ],
                                 ),
-                                child: Container(
-                                  child: CircleAvatar(
-                                    radius: 48,
-                                    child: Image.network(
-                                      center.urlLogoImage,
+                                width: double.infinity,
+                                height: MediaQuery.of(context).size.height / 7,
+                                child: Padding(
+                                  padding: const EdgeInsets.all(5.0),
+                                  child: Container(
+                                    child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceAround,
+                                      children: [
+                                        CircleAvatar(
+                                          radius: MediaQuery.of(context)
+                                                  .size
+                                                  .height /
+                                              14, // half of the height
+                                          backgroundImage:
+                                              NetworkImage(center.urlLogoImage),
+                                        ),
+                                        Container(
+                                          child: Column(
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.start,
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.center,
+                                            children: [
+                                              Text(
+                                                center.name,
+                                                style: TextStyle(
+                                                  fontSize: 18,
+                                                  fontWeight: FontWeight.bold,
+                                                ),
+                                              ),
+                                              SizedBox(height: 8),
+                                              Row(
+                                                mainAxisAlignment:
+                                                    MainAxisAlignment.start,
+                                                crossAxisAlignment:
+                                                    CrossAxisAlignment.center,
+                                                children: [
+                                                  Text("(Review) "),
+                                                  Icon(
+                                                    FontAwesomeIcons.solidStar,
+                                                    size: 13,
+                                                    color: Color.fromARGB(
+                                                        255, 241, 241, 47),
+                                                  ),
+                                                ],
+                                              ),
+                                              SizedBox(height: 8),
+                                              Row(
+                                                mainAxisAlignment:
+                                                    MainAxisAlignment.start,
+                                                children: [
+                                                  Row(
+                                                    children: [
+                                                      Icon(
+                                                        FontAwesomeIcons.clock,
+                                                        size: 13,
+                                                      ),
+                                                      Text(" " +
+                                                          center
+                                                              .operatingHours),
+                                                    ],
+                                                  )
+                                                ],
+                                              )
+                                            ],
+                                          ),
+                                        ),
+                                      ],
                                     ),
                                   ),
                                 ),
                               ),
-                              Container(
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    Text(
-                                      center.name,
-                                      style: TextStyle(
-                                        fontSize: 18,
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                    ),
-                                    SizedBox(height: 10),
-                                    Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.start,
-                                      children: [
-                                        Row(
-                                          children: [
-                                            Icon(
-                                              FontAwesomeIcons.solidStar,
-                                              size: 13,
-                                              color: Colors.yellowAccent,
-                                            ),
-                                            Text("4.5")
-                                          ],
-                                        ),
-                                        Row(
-                                          children: [
-                                            Icon(
-                                              FontAwesomeIcons.clock,
-                                              size: 13,
-                                            ),
-                                            Text(
-                                              center.operatingHours,
-                                            ),
-                                          ],
-                                        )
-                                      ],
-                                    )
-                                  ],
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
+                            ),
+                          );
+                        },
+                      );
+                    },
                   ),
-                );
-              },
-            );
-          },
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
